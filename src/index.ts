65b280a39427829e3ebb171f0b92623c3d051a7d
DJaegerScript <adjiedjaka0902@gmail.com>
2022-07-06 08:55:17 +0700

    added init command

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
 #!/usr/bin/env node
 
 import kleur from 'kleur'
+import { Command } from 'commander'
 
+// @ts-ignore
+import { version } from '../package.json'
 import striveInit from './initialization'
 
 const main = (argv: string[]) => {
+  const program = new Command()
   try {
-    striveInit()
+    program
+      .name('strive')
+      .usage('<command>')
+      .description(
+        'CLI to generate well-structured next.js folder architecture'
+      )
+      .version(`next-strive@${version}`, '-v, --version')
+
+    striveInit(program)
+
+    program.showHelpAfterError().parse()
+
+    // const limit = options.first ? 1 : undefined
+    // console.log(program.args[0].split(options.separator, limit))
+    // striveInit()
   } catch (error: any) {
     console.error(
       kleur.bold().bgRed().yellow('ERROR'),
--- src/initialization/index.ts
+++ src/initialization/index.ts
@@ -1,6 +1,8 @@
 import path from 'path'
 import fs from 'fs-extra'
 import { execSync } from 'child_process'
+import { Command } from 'commander'
+
 import {
   generateComponents,
   generateLinter,
@@ -8,24 +10,28 @@ import {
   updatePackage,
 } from './functions'
 
-const striveInit = () => {
-  const rootDir = process.cwd()
-  const nextConfig = path.join(rootDir, 'next.config.js')
-  const srcDir = path.join(rootDir, 'src')
+const striveInit = (program: Command) =>
+  program
+    .command('init')
+    .description('Initialize strive scaffolding')
+    .action(() => {
+      const rootDir = process.cwd()
+      const nextConfig = path.join(rootDir, 'next.config.js')
+      const srcDir = path.join(rootDir, 'src')
 
-  fs.chmodSync(rootDir, fs.constants.S_IRWXU)
+      fs.chmodSync(rootDir, fs.constants.S_IRWXU)
 
-  if (!fs.existsSync(nextConfig))
-    throw new Error('Strive init failed, project is not a next app')
+      if (!fs.existsSync(nextConfig))
+        throw new Error('Strive init failed, project is not a next app')
 
-  moveFiles(srcDir)
-  generateComponents(srcDir)
-  generateLinter(rootDir)
-  updatePackage(rootDir)
+      moveFiles(srcDir)
+      generateComponents(srcDir)
+      generateLinter(rootDir)
+      updatePackage(rootDir)
 
-  execSync(
-    "npm i && npx husky install && npx husky add .husky/pre-commit 'npm run test-all' && npm run test-all"
-  )
-}
+      execSync(
+        "npm i && npx husky install && npx husky add .husky/pre-commit 'npm run test-all' && npm run test-all"
+      )
+    })
 
 export default striveInit
