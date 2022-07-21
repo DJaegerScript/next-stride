80b09263bf305b7f5fd977a7e83bd27ce7f5a188
DJaegerScript <adjiedjaka0902@gmail.com>
2022-07-21 07:17:17 +0700

    added page option on generate function

--- src/generate/index.ts
+++ src/generate/index.ts
@@ -1,13 +1,25 @@
 import { Command } from 'commander'
-import generateFunction from './functions'
+import generateFunction, { Schematic } from './functions'
+
+export interface GenerateOptions {
+  P: string
+}
 
 const striveGenerate = (program: Command) =>
   program
     .command('generate [schematic] [name]')
     .alias('g')
     .description('Generate specified schematics')
-    .action((schematic: string, name: string) =>
-      generateFunction(schematic, name.toLowerCase())
+    .option(
+      '--pages, -p <pages>',
+      'specify pages to be generated, used along module schematic'
     )
+    .action((schematic: Schematic, name: string, options: GenerateOptions) => {
+      if (schematic === 'module' && options.P === undefined) {
+        throw new Error('You must provide -p flag to generate modules')
+      }
+
+      return generateFunction(schematic, name.toLowerCase(), options)
+    })
 
 export default striveGenerate
