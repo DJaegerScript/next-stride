31f5570ae7686a58575b61613f10e65dd6293f86
DJaegerScript <adjiedjaka0902@gmail.com>
2022-07-28 06:57:40 +0700

    added no-page opt & make page opt optional

--- src/generate/index.ts
+++ src/generate/index.ts
@@ -15,12 +15,9 @@ const striveGenerate = (program: Command) =>
       'specify pages to be generated, used along module schematic'
     )
     .option('--ssr', 'generate ssr function for module schematics')
-    .action((schematic: Schematic, name: string, options: GenerateOptions) => {
-      if (schematic === 'module' && options.P === undefined) {
-        throw new Error('You must provide -p flag to generate modules')
-      }
-
-      return generateFunction(schematic, name.toLowerCase(), options)
-    })
+    .option('--no-page', 'prevent module schematics to generate pages')
+    .action((schematic: Schematic, name: string, options: GenerateOptions) =>
+      generateFunction(schematic, name.toLowerCase(), options)
+    )
 
 export default striveGenerate
