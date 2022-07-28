616589339f6ce9249da1f7facf304c806da58e05
DJaegerScript <adjiedjaka0902@gmail.com>
2022-07-28 07:00:13 +0700

    generateModulePageContent -> generatePageContent

---
+++ src/generate/functions/generateModule/generatePageContent.ts
@@ -0,0 +1,21 @@
+const generatePageContent = (
+  name: string,
+  moduleName: string,
+  SSRName: string | null,
+  fileType: string
+) => {
+  return `import React from 'react'
+import { ${moduleName} } from '@modules'
+${fileType === '.ts' ? "import type { NextPage } from 'next'" : ''}
+${SSRName ? `import { ${SSRName} } from '@ssr'` : ''}
+
+const ${name}${fileType === '.ts' ? ': NextPage' : ''} = (${
+    SSRName ? 'props' : ''
+  }) => <${moduleName} ${SSRName ? '{...props}' : ''}/>
+
+${SSRName ? `export const getServerSideProps = ${SSRName}` : ''}
+
+export default ${name}`
+}
+
+export default generatePageContent
